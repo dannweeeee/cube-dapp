48b8a9880ef7161d9df0746f553e4cd376f323bc
Dann Wee <dannweezj@gmail.com>
2024-10-14 00:10:15 +0800

    feat: update next config for fleek

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    },
+    trailingSlash: true,
     images: {
         remotePatterns: [
             {
